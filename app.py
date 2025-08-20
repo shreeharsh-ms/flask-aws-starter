90be004edc94f1ac43ee55685577ccbf88687c1c
Shreeharsh <info.shreeharshshivpuje@gmail.com>
2025-08-21 02:03:27 +0530

    Created a CD pipeline That Pushes the code to EC2

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ def health():
     return {"status": "ok"}
 
 if __name__ != '__main__':
+    print("Running in production mode")
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
