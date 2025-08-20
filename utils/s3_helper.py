3a3e328fc3c345180c4d80341f75220d2be0ec07
Shreeharsh <info.shreeharshshivpuje@gmail.com>
2025-08-21 00:13:17 +0530

    Created a CD pipeline That Pushes the code to EC2

--- utils/s3_helper.py
+++ utils/s3_helper.py
@@ -9,8 +9,8 @@ S3_BUCKET = os.getenv("S3_BUCKET", "flask-app-images-dev")
 s3_client = boto3.client("s3", region_name=AWS_REGION)
 
 def upload_file_to_s3(file_obj, filename):
-    """Upload file object to S3"""
-    s3_client.upload_fileobj(file_obj, S3_BUCKET, filename, ExtraArgs={"ACL": "public-read"})
+    """Upload file object to S3 without ACL"""
+    s3_client.upload_fileobj(file_obj, S3_BUCKET, filename)
     return f"https://{S3_BUCKET}.s3.{AWS_REGION}.amazonaws.com/{filename}"
 
 def list_files_in_s3():
